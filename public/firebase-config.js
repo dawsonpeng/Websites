d49f0d271844e682cde3a2a494382f2390f2e5e8
Dawson <dpeng1@hwemail.com>
2025-11-04 08:36:07 -0800

    added global loss tracking

---
+++ public/firebase-config.js
@@ -0,0 +1,7 @@
+window.__FIREBASE_CONFIG__ = {
+    apiKey: "AIzaSyBzptt459G8eT0VMJs4TVcVcjoy1QHHxSA",
+    authDomain: "pokemon-f1ef9.firebaseapp.com",
+    projectId: "pokemon-f1ef9",
+    appId: "1:1059820719675:web:e49bbd5e5b119bc8c9961e"
+    // measurementId/storageBucket optional
+  };
